a12884d779ca6df39f39b5be2c31a5165aba2472
iiMox <iimox04@gmail.com>
2024-02-23 11:29:47 +0100

    Fix Pressure Reading Not Recorded + Add Milliseconds to Time

--- Watch Apps/data_collections_code_interval_approach.js
+++ Watch Apps/data_collections_code_interval_approach.js
@@ -1,5 +1,5 @@
 const f = require("Storage").open("datalogs.txt", "w");
-f.write("Time,Timestamp,X,Y,Z,Magnitue\n");
+f.write("Time,Timestamp,X,Y,Z,Magnitude\n");
 
 const f1 = require("Storage").open("pressurelogs.txt", "w");
 f1.write("Time,Timestamp,Pressure\n");
@@ -11,26 +11,32 @@ const recordAccel = () => {
     const accel = Bangle.getAccel();
     const time = new Date();
     const t1 = getTime() - start;
-    t =
-        ("0" + time.getHours()).slice(-2) +
-        ":" +
-        ("0" + time.getMinutes()).slice(-2) +
-        ":" +
-        ("0" + time.getSeconds()).slice(-2);
+    t = `${time.getHours().toString().padStart(2, 0)}:${time
+        .getMinutes()
+        .toString()
+        .padStart(2, 0)}:${time.getSeconds().toString().padStart(2, 0)}.${time
+        .getMilliseconds()
+        .toString()
+        .padStart(3, 0)}`;
     f.write([t, t1, accel.x, accel.y, accel.z, accel.mag].join(",") + "\n");
 };
 
 const recordPressure = () => {
-    const pressure = Bangle.getPressure();
-    const time = new Date();
-    const t2 = getTime() - start;
-    t =
-        ("0" + time.getHours()).slice(-2) +
-        ":" +
-        ("0" + time.getMinutes()).slice(-2) +
-        ":" +
-        ("0" + time.getSeconds()).slice(-2);
-    f1.write([t, t2, pressure.pressure].join(",") + "\n");
+    Bangle.getPressure().then((pressure) => {
+        const time = new Date();
+        const t2 = getTime() - start;
+        t = `${time.getHours().toString().padStart(2, 0)}:${time
+            .getMinutes()
+            .toString()
+            .padStart(2, 0)}:${time
+            .getSeconds()
+            .toString()
+            .padStart(2, 0)}.${time
+            .getMilliseconds()
+            .toString()
+            .padStart(3, 0)}`;
+        f1.write([t, t2, pressure.pressure].join(",") + "\n");
+    });
 };
 
 setInterval(() => {
