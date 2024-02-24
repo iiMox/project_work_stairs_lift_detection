71971b231f7aa0f48c45dd61397d2c217fd8ba81
iiMox <iimox04@gmail.com>
2024-02-24 13:41:54 +0100

    Merge Accel & Pressure Reading In One File

--- Watch Apps/data_collections_code_interval_approach.js
+++ Watch Apps/data_collections_code_interval_approach.js
@@ -1,30 +1,14 @@
 const f = require("Storage").open("datalogs.txt", "w");
-f.write("Time,Timestamp,X,Y,Z,Magnitude\n");
-
-const f1 = require("Storage").open("pressurelogs.txt", "w");
-f1.write("Time,Timestamp,Pressure\n");
+f.write("Time,Timestamp,X,Y,Z,Magnitude,Pressure\n");
 
 Bangle.setBarometerPower(true);
 var start = getTime();
 
-const recordAccel = () => {
-    const accel = Bangle.getAccel();
-    const time = new Date();
-    const t1 = getTime() - start;
-    t = `${time.getHours().toString().padStart(2, 0)}:${time
-        .getMinutes()
-        .toString()
-        .padStart(2, 0)}:${time.getSeconds().toString().padStart(2, 0)}.${time
-        .getMilliseconds()
-        .toString()
-        .padStart(3, 0)}`;
-    f.write([t, t1, accel.x, accel.y, accel.z, accel.mag].join(",") + "\n");
-};
-
-const recordPressure = () => {
+const recordReadings = () => {
     Bangle.getPressure().then((pressure) => {
+        const accel = Bangle.getAccel();
         const time = new Date();
-        const t2 = getTime() - start;
+        const t1 = getTime() - start;
         t = `${time.getHours().toString().padStart(2, 0)}:${time
             .getMinutes()
             .toString()
@@ -35,11 +19,20 @@ const recordPressure = () => {
             .getMilliseconds()
             .toString()
             .padStart(3, 0)}`;
-        f1.write([t, t2, pressure.pressure].join(",") + "\n");
+        f.write(
+            [
+                t,
+                t1,
+                accel.x,
+                accel.y,
+                accel.z,
+                accel.mag,
+                pressure.pressure,
+            ].join(",") + "\n"
+        );
     });
 };
 
 setInterval(() => {
-    recordAccel();
-    recordPressure();
+    recordReadings();
 }, 20);
